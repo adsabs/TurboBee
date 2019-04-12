0aa34c7d9bdaca1020c772de5f929b7329adac1c
Sergi Blanco-Cuaresma <marblestation@users.noreply.github.com>
2019-04-12 14:27:54 -0400

    Pg module with a new method

--- turbobee/pg.lua
+++ turbobee/pg.lua
@@ -1,10 +1,15 @@
-pgmoon = require("pgmoon")
-pg = pgmoon.new({
-    host = os.getenv("DATABASE_HOST"),
-    port = os.getenv("DATABASE_PORT"),
-    database = os.getenv("DATABASE_NAME"),
-    user = os.getenv("DATABASE_USER"),
-    password = os.getenv("DATABASE_PASSWORD")
-})
+local M = {}
+local pgmoon = require("pgmoon")
 
-return pg
+function M.new()
+    local pg = pgmoon.new({
+        host = os.getenv("DATABASE_HOST"),
+        port = os.getenv("DATABASE_PORT"),
+        database = os.getenv("DATABASE_NAME"),
+        user = os.getenv("DATABASE_USER"),
+        password = os.getenv("DATABASE_PASSWORD")
+    })
+    return pg
+end
+
+return M
